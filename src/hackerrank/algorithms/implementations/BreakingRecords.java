75a14a3733482357e5142020c26bc3030257cdde
ngocnv <nguyenngoc101@yahoo.com>
2018-04-13 21:07:24 +0700

    day9 recursion

---
+++ src/hackerrank/algorithms/implementations/BreakingRecords.java
@@ -0,0 +1,33 @@
+package hackerrank.algorithms.implementations;
+
+import java.util.Scanner;
+
+public class BreakingRecords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int max = -999;
+        int min = Integer.MAX_VALUE;
+        int countMax = -1;
+        int countMin = -1;
+        int current;
+
+        for (int i = 0; i < n; i++) {
+            current = scanner.nextInt();
+            if(current > max) {
+                max = current;
+                countMax++;
+                System.out.println("max: "+i);
+            }
+            if (current < min) {
+                min = current;
+                countMin++;
+                System.out.println("min: "+i);
+            }
+        }
+        System.out.println(countMax + " "+countMin);
+
+    }
+
+
+}
