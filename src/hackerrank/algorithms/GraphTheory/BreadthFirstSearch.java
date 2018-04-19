ac05619afbdb92941c1baacdd7517fea9e0640aa
ngocnv <nguyenngoc101@yahoo.com>
2018-04-19 22:19:40 +0700

    fix breath first search

--- src/hackerrank/algorithms/GraphTheory/BreadthFirstSearch.java
+++ src/hackerrank/algorithms/GraphTheory/BreadthFirstSearch.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class BreadthFirstSearch {
 
-
         static int[] bfs(int n, int m, int[][] edges, int s) {
             int[][] graph = new int[n][n];
             for (int i = 0; i < n; i++) {
@@ -14,40 +13,40 @@ public class BreadthFirstSearch {
             }
             for (int i = 0; i < edges.length; i++) {
                 graph[edges[i][0]-1][edges[i][1]-1] = 6;
+                graph[edges[i][1]-1][edges[i][0]-1] = 6;
             }
 
             // Complete this function
             int[] distances = new int[n];
+            boolean[] calculated = new boolean[n];
+
             for (int i = 0; i < n; i++) {
                 distances[i] = 0;
             }
 
-            System.out.println("---calculate------");
             LinkedList<Integer> queue = new LinkedList<Integer>();
             queue.add(s-1);
+            calculated[s-1] = true;
             Set<Integer> traversaled = new HashSet<>();
             for (int i = 0; i < n; i++) {
                 traversaled.add(i);
             }
             while (!queue.isEmpty()) {
-                System.out.println("queue size: "+queue.size());
-                int currentEdge = queue.pop();
+                int currentEdge = queue.remove(0);
                 for (int i = 0; i < n; i++) {
-                    if (traversaled.contains(i) && graph[currentEdge][i] != -1) {
-                        distances[i] = distances[currentEdge] + graph[currentEdge][i];
+                    if (traversaled.contains(i) && (graph[currentEdge][i] != -1)) {
+                        if (!calculated[i]) {
+                            distances[i] = distances[currentEdge] + graph[currentEdge][i];
+                            calculated[i] = true;
+                        }
                         queue.add(i);
                     }
                 }
                 traversaled.remove(currentEdge);
             }
 
-            System.out.println("--- finish calculate------");
-
             return Arrays.stream(distances).map(e -> e == 0 ? -1 : e).toArray();
-
-
         }
-
 
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
@@ -71,6 +70,4 @@ public class BreadthFirstSearch {
             }
             in.close();
         }
-
-
 }
