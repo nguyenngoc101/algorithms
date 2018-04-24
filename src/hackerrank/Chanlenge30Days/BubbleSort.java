73df13948290b493a62cf749c940bff6634c878b
ngocnv <nguyenngoc101@yahoo.com>
2018-04-24 22:37:59 +0700

    day 20 code challenge buble sort

--- src/hackerrank/Chanlenge30Days/BubbleSort.java
+++ src/hackerrank/Chanlenge30Days/BubbleSort.java
@@ -1,23 +1,36 @@
 package hackerrank.Chanlenge30Days;
 
+import java.util.Scanner;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arrs = {2,3,4,5,1,2,3,4};
-        sort(arrs);
-        for(int i=0; i< arrs.length; i++){
-            System.out.println(arrs[i]);
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
         }
+        System.out.println("Array is sorted in " + sort(a) + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " +a[a.length-1]);
     }
 
-    public static void sort(int[] a){
+    public static int sort(int[] a){
+        int numberOfSwaps = 0;
         for(int i= 0; i< a.length; i++){
-            for (int j= 0; j< a.length-1; j++){
-                if(a[i] < a[j]){
-                    int tem = a[i];
-                    a[i] = a[j];
-                    a[j] = tem;
+            for (int j= 0; j< a.length-i-1; j++){
+                if(a[j] > a[j+1]){
+                    int tem = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tem;
+                    numberOfSwaps++;
                 }
             }
+
+            if (numberOfSwaps == 0) {
+                break;
+            }
         }
+        return numberOfSwaps;
     }
 }
