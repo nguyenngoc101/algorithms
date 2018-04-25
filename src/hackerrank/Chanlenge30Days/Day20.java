a57c8869bcdd7c1110b73140e8824544772a6ce5
ngocnv <nguyenngoc101@yahoo.com>
2018-04-25 21:56:26 +0700

    day 21 code challenge generics programming

---
+++ src/hackerrank/Chanlenge30Days/Day20.java
@@ -0,0 +1,37 @@
+package hackerrank.Chanlenge30Days;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day20 {
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+
+    static class Printer <T> {
+
+        public void printArray(T[] arr) {
+            Arrays.stream(arr).forEach(System.out::println);
+        }
+    }
+}
