e13177cc195574e67c405513f4fb32a7ab4a006b
nguyenngoc101 <nguyenngoc101@yahoo.com>
2018-11-04 22:07:09 +0700

    hashtable: RansomNote

--- src/interview/hashtable/RansomNote.java
+++ src/interview/hashtable/RansomNote.java
@@ -1,4 +1,27 @@
 package interview.hashtable;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class RansomNote {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        String magazine = scanner.nextLine();
+        String note = scanner.nextLine();
+        Map<String, Long> magazines = Arrays.stream(magazine.split(" "))
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        boolean canCopy = true;
+        for (String word : note.split(" ")) {
+            Long appears = magazines.get(word);
+            if (appears == null || appears <= 0) {
+                canCopy = false;
+                break;
+            }
+            magazines.put(word, appears - 1);
+        }
+        System.out.println(canCopy==true ? "Yes" : "No");
+    }
 }
